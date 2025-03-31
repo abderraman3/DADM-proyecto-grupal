57c7f3af4dc8b3fafb9e15dfdd9b18d1b3f93373
JorgeRomeroSevillano <j.romero.sevillano@gmail.com>
2025-03-31 16:58:20 +0200

    app se ejecuta

--- app/src/main/java/dadm/jromsev/sportnew/ui/MainActivity.kt
+++ app/src/main/java/dadm/jromsev/sportnew/ui/MainActivity.kt
@@ -1,18 +1,22 @@
-package dadm.jromsev.sportnew
+package dadm.jromsev.sportnew.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dadm.jromsev.sportnew.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or
+                        WindowInsetsCompat.Type.displayCutout() )
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
